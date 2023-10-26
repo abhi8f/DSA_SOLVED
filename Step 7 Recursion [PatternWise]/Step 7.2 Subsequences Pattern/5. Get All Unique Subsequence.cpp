937ad521e12195d1699f1c84c77303db1f9143ac
Abhishek N N <7abhisheknn@gmail.com>
2023-10-26 14:09:50 +0530

    update

--- Step 7 Recursion [PatternWise]/Step 7.2 Subsequences Pattern/5. Get All Unique Subsequence.cpp
+++ Step 7 Recursion [PatternWise]/Step 7.2 Subsequences Pattern/5. Get All Unique Subsequence.cpp
@@ -1,41 +1,20 @@
-/*
-    Time complexity: O( k * (2 ^ n) )
-    Space complexity: O( k * (2 ^ n) )
-    
-    Where 'k' is the average size of a subset and 
-    'n' is the size of the array.
-*/
-
-// Helper function to generate all unique subsets
-void findSubsets(int index, vector<int>& arr, vector<int>& temp, vector<vector<int>>& ans) {
-    // Pushing the array 'temp' into 'ans'
-    ans.push_back(temp);
-    
-    for(int i = index; i < (int)arr.size(); ++i) {
-        // Checking if the previous element is same as current element
-        if(i != index && arr[i] == arr[i - 1]) continue;
-        
-        // Inserting the current element in array 'temp'
-        temp.push_back(arr[i]);
-        
-        findSubsets(i + 1, arr, temp, ans);
-        
-        // Removing last element from 'temp'
-        temp.pop_back();
+class Solution {
+private:
+    void getSubsets(int i, int n, vector<int> &nums, vector<int> &cur, vector<vector<int>> &ans) {
+        ans.push_back(cur);
+        for(int j = i; j < n; j++) {
+            if (j!=i && nums[j]==nums[j-1]) continue;
+            cur.push_back(nums[j]);
+            getSubsets(j+1, n, nums, cur, ans);
+            cur.pop_back();
+        }
     }
-}
-
-
-vector<vector<int>> getUniqueSubsets(vector<int>& arr) {
-    // Initializing an 2-D array 'ans'
-    // to store the subsets
-    vector<vector<int>> ans;
-
-    // Initializing an array 'temp' which will 
-    // store the elements to be inserted in the 'ans'
-    // as an array
-    vector<int> temp;
-    
-    findSubsets(0, arr, temp, ans);
-    return ans;
-}
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> cur;
+        getSubsets(0, nums.size(), nums, cur, ans);
+        return ans;
+    }
+};
